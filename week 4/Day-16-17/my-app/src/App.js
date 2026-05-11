741a464f698d8d22255b6232874082fa43adf315
ShaikMunafSharif <shaikmunafsharif786@gmial.com>
2026-05-11 09:07:44 +0530

    Added Week 4 React projects and updated Week 3

---
+++ week 4/Day-16-17/my-app/src/App.js
@@ -0,0 +1,27 @@
+function App() {
+
+    const fruits = ["Apple", "Banana", "Mango"];
+
+    return (
+
+        <div>
+
+            <h1>Fruit List</h1>
+
+            <ul>
+
+                {fruits.map((fruit, index) => (
+                    <li key={index}>
+                        {fruit}
+                    </li>
+                ))}
+
+            </ul>
+
+        </div>
+
+    );
+
+}
+
+export default App;
